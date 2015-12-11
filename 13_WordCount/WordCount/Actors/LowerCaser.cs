9830e52918591a74d531314abff2b0d84177e192
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-12-11 20:59:07 +0100

    added files for 13th Dec.

---
+++ 13_WordCount/WordCount/Actors/LowerCaser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Akka.Actor;
+using WordCount.Messages;
+
+namespace WordCount
+{
+    /// <summary>
+    /// Convert a string stream into lower case
+    /// </summary>
+    public class LowerCaser : ReceiveActor
+    {
+        public LowerCaser(IActorRef next)
+        {
+            Receive<string>(s => next.Tell(s.ToLower()));
+            Receive<End>(next.Tell);
+        }
+    }
+}
---
+++ 13_WordCount/WordCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+using WordCount.Actors;
+
+namespace WordCount
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("Words");
+
+            var console = system.ActorOf(Props.Create<ConsoleWriter>());
+            var counter = system.ActorOf(Props.Create<WordCounter>(console));
+            var splitter = system.ActorOf(Props.Create<WordSplitter>(counter));
+            var caser = system.ActorOf(Props.Create<LowerCaser>(splitter));
+            system.ActorOf(Props.Create<FileReader>(args[0], caser));
+
+            system.AwaitTermination();
+        }
+    }
+}
